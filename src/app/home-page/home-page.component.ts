d85a5629f8447d6bf18218786fe66bf8da31db29
Gabriela Parra <parragabriela@icloud.com>
2017-07-21 15:36:49 -0400

    added sign up and sign in forms

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthServiceService } from '../services/auth-service.service';
 
 @Component({
   selector: 'app-home-page',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+  fullNameValue: string;
+  emailValue: string;
+  passwordValue: string;
+  errorMessage: string;
 
-  constructor() { }
+  loginEmail: string;
+  loginPassword: string;
+
+  constructor(
+    private authThing: AuthServiceService,
+    private routerThing: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  doSignUp() {
+    this.authThing.signup(this.fullNameValue, this.emailValue, this.passwordValue)
+      .then((resultFromApi) => {
+        this.fullNameValue = "";
+        this.emailValue = "";
+        this.passwordValue = "";
+        this.errorMessage = "";
+
+        //redirect to camels
+        this.routerThing.navigate(['/camels']);
+      })
+      .catch((err) => {
+        const parsedError = err.json();
+        this.errorMessage = parsedError.message;
+      })
+  }
+
+  doLogin() {
+    alert('login submitted');
+  }
 }
